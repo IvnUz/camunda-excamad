ef8b69ad115b14f355fa8dcef38d98c9d004250f
Антонов Иван Юрьевич <iantonov@systematica-consulting.ru>
2023-08-28 06:16:41 +0300

    add pagination to incindent view; incident retry change from job to external-task api

--- src/components/Incident.vue
+++ src/components/Incident.vue
@@ -8,12 +8,13 @@
       :color="'#007bff'"
     />
 
-    <h2>Incidents count: {{incidentsToShow.length}}</h2>
+    <h2>Incidents count: {{ totalIncidentCount }}</h2>
     <b-form-checkbox
       id="checkbox1"
       name="checkbox1"
       v-model="hideParentIncidents"
-    >Hide parent incidents</b-form-checkbox>
+      >Hide parent incidents</b-form-checkbox
+    >
     <div v-if="incidents.length != 0">
       <b-form class="mb-2" inline>
         <b-form-input
@@ -23,16 +24,22 @@
         ></b-form-input>
 
         <datalist id="my-list-id">
-          <option v-bind:key="item" v-for="item in filterFailedActivity">{{ item }}</option>
+          <option v-bind:key="item" v-for="item in filterFailedActivity">
+            {{ item }}
+          </option>
         </datalist>
 
-        <b-form-input class="ml-1" v-model="countOfJobs" type="number"></b-form-input>
-        <b-btn class="ml-1" variant="warning" @click="getFistNJobs">Rerun {{countOfJobs}} jobs</b-btn>
-        <b-btn
+        <b-form-input
           class="ml-1"
-          variant="danger"
-          @click="healAndRetry"
-        >Rerun all activities</b-btn>
+          v-model="countOfJobs"
+          type="number"
+        ></b-form-input>
+        <b-btn class="ml-1" variant="warning" @click="getFistNJobs"
+          >Rerun {{ countOfJobs }} jobs</b-btn
+        >
+        <b-btn class="ml-1" variant="danger" @click="healAndRetry"
+          >Rerun all activities</b-btn
+        >
       </b-form>
       <small>
         <div class="row">
@@ -54,26 +61,35 @@
                 </thead>
                 <tbody>
                   <tr :key="item.id" v-for="item in incidentsToShow">
-                    <td style="word-break:break-all;">{{item.activityId}}</td>
-                    <td
-                      style="word-break:break-all;"
-                    >{{ item.incidentMessage ? item.incidentMessage.substring(0,100)+'...' : ''}}</td>
-                    <td>{{item.rootCauseIncidentId}}</td>
-                    <td>{{item.causeIncidentId}}</td>
-                    <td
-                      style="word-break:break-all;"
-                    >{{convertDateToHumanStyle(item.incidentTimestamp)}}</td>
-                    <td style="word-break:break-all;">{{item.processDefinitionId}}</td>
-                    <td style="word-break:break-all;">
+                    <td style="word-break: break-all">{{ item.activityId }}</td>
+                    <td style="word-break: break-all">
+                      {{
+                        item.incidentMessage
+                          ? item.incidentMessage.substring(0, 100) + "..."
+                          : ""
+                      }}
+                    </td>
+                    <td>{{ item.rootCauseIncidentId }}</td>
+                    <td>{{ item.causeIncidentId }}</td>
+                    <td style="word-break: break-all">
+                      {{ convertDateToHumanStyle(item.incidentTimestamp) }}
+                    </td>
+                    <td style="word-break: break-all">
+                      {{ item.processDefinitionId }}
+                    </td>
+                    <td style="word-break: break-all">
                       <router-link
-                        :to="{name:'processdetail', params:{ processInstanceId: item.processInstanceId}}"
+                        :to="{
+                          name: 'processdetail',
+                          params: { processInstanceId: item.processInstanceId },
+                        }"
                       >
                         <p class="card-text">
-                          <b>{{item.processInstanceId}}</b>
+                          <b>{{ item.processInstanceId }}</b>
                         </p>
                       </router-link>
                     </td>
-                    <td style="word-break:break-all;">
+                    <td style="word-break: break-all">
                       <b-btn
                         size="sm"
                         class="ml-2"
@@ -83,7 +99,7 @@
                         <font-awesome-icon icon="redo" />
                       </b-btn>
                     </td>
-                    <td style="word-break:break-all;">
+                    <td style="word-break: break-all">
                       <b-btn
                         size="sm"
                         class="ml-2"
@@ -101,12 +117,18 @@
         </div>
       </small>
     </div>
-    <div
-      v-if="incidents.length == 0 && ready==true"
-      class="alert alert-primary"
-      role="alert"
-    >No incidents!</div>
+    <div v-if="totalIncidentCount === 0 && ready">
+      <div class="alert alert-primary" role="alert">No incidents!</div>
+    </div>
     <hr />
+    <div class="pagination">
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="totalIncidentCount"
+        :per-page="incidentsPerPage"
+        aria-controls="incident"
+      ></b-pagination>
+    </div>
   </div>
 </template>
 
@@ -137,12 +159,14 @@ export default {
         executable: false,
         withException: true,
         noRetriesLeft: true,
-        active: false
+        active: false,
       },
       jobQuerySelected: {
         processInstanceId: [],
-        activityId: []
-      }
+        activityId: [],
+      },
+      currentPage: 1,
+      incidentsPerPage: 100,
     };
   },
   watch: {
@@ -151,33 +175,64 @@ export default {
         return obj.globalRoot === true;
       });
       if (newValue == true) {
-        this.incidentsToShow = this.incidentsGlobalRoot;
+        this.incidentsToShow = this.incidentsGlobalRoot.slice(
+          (this.currentPage - 1) * this.incidentsPerPage,
+          this.currentPage * this.incidentsPerPage
+        );
       }
 
       if (newValue == false) {
-        this.incidentsToShow = this.incidents;
+        this.incidentsToShow = this.incidents.slice(
+          (this.currentPage - 1) * this.incidentsPerPage,
+          this.currentPage * this.incidentsPerPage
+        );
       }
-    }
+    },
+    currentPage(newPage) {
+      // Update the incidents shown on the current page based on hideParentIncidents value
+      if (this.hideParentIncidents) {
+        this.incidentsToShow = this.incidentsGlobalRoot.slice(
+          (newPage - 1) * this.incidentsPerPage,
+          newPage * this.incidentsPerPage
+        );
+      } else {
+        this.incidentsToShow = this.incidents.slice(
+          (newPage - 1) * this.incidentsPerPage,
+          newPage * this.incidentsPerPage
+        );
+      }
+    },
   },
   methods: {
-
     getAllIncidents() {
       api
         .getEntity("incident", "", "sortBy=incidentTimestamp&sortOrder=desc")
-        .then(value => {
+        .then((value) => {
           this.incidents = value;
           var vm = this;
           this.ready = true;
-          this.incidents.forEach(incident => {
+          this.incidents.forEach((incident) => {
             if (!incident.incidentMessage) {
               vm.$set(incident, "globalRoot", false);
             } else {
               vm.$set(incident, "globalRoot", true);
-              this.incidentsToShow.push(incident);
             }
           });
-          this.incidentsToShow = this.incidents;
+          this.updateIncidentsToShow();
         });
+    },
+    updateIncidentsToShow() {
+      const startIndex = (this.currentPage - 1) * this.incidentsPerPage;
+      const endIndex = startIndex + this.incidentsPerPage;
+
+      if (this.hideParentIncidents) {
+        this.incidentsToShow = this.incidentsGlobalRoot.slice(
+          startIndex,
+          endIndex
+        );
+      } else {
+        this.incidentsToShow = this.incidents.slice(startIndex, endIndex);
+      }
     },
     updateSingleJobRetry(item) {
       this.jobQuerySelected.processInstanceId = [];
@@ -185,9 +240,8 @@ export default {
       this.jobQuerySelected.processInstanceId = item.processInstanceId;
       this.jobQuerySelected.activityId = item.activityId;
       this.$api()
-        .post("/job/retries", {
-          retries: this.retries,
-          jobQuery: this.jobQuerySelected
+        .put("/external-task/"+item.configuration+"/retries", {
+          retries: 1
         })
         .then(() => {
           var index = this.incidents.indexOf(item);
@@ -197,8 +251,16 @@ export default {
           this.$notify({
             group: "foo",
             title: "Rerun started",
-            type: "info"
+            type: "info",
           });
+        }).catch((error) => {
+          if (error) {
+            this.$notify({
+              group: "foo",
+              title: "Single job Rerun cant start :(",
+              type: "error",
+            });
+          }
         });
     },
     healAndRetry() {
@@ -209,31 +271,29 @@ export default {
       }, 5000);
     },
     convertDateToHumanStyle: function (date) {
-      return this.$momenttrue(date)
-        .startOf("second")
-        .fromNow();
+      return this.$momenttrue(date).startOf("second").fromNow();
     },
     retryAllIncidents() {
       this.$api()
         .post("/job/retries/", {
           retries: this.retries,
-          jobQuery: this.jobQuery
+          jobQuery: this.jobQuery,
         })
-        .then(response => {
+        .then((response) => {
           if (response) {
             this.$notify({
               group: "foo",
               title: "Rerun started",
-              type: "info"
+              type: "info",
             });
           }
         })
-        .catch(error => {
+        .catch((error) => {
           if (error) {
             this.$notify({
               group: "foo",
               title: "Rerun cant start :(",
-              type: "error"
+              type: "error",
             });
           }
         });
@@ -241,13 +301,13 @@ export default {
     DeleteProccessInstance(item) {
       this.$api()
         .delete("/process-instance/" + item.processInstanceId)
-        .then(response => {
+        .then((response) => {
           if (response) {
             this.$notify({
               group: "foo",
               title: "Instance deleted",
               text: item.processInstanceId,
-              type: "success"
+              type: "success",
             });
           }
           var index = this.incidents.indexOf(item);
@@ -255,78 +315,88 @@ export default {
             this.incidents.splice(index, 1);
           }
         })
-        .catch(error => {
+        .catch((error) => {
           this.$notify({
             group: "foo",
             title: "Instance NOT deleted",
             text: error,
-            type: "error"
+            type: "error",
           });
         });
     },
     getFistNJobs() {
-      if (this.countOfJobs > 100) {
-        this.countOfJobs = 100
+      if (this.countOfJobs > 500) {
+        this.countOfJobs = 500;
       }
       this.jobsIds = [];
       var postBody = {
         withException: true,
         noRetriesLeft: true,
-        activityId: this.activityIdForJobSearch
+        activityId: this.activityIdForJobSearch,
       };
-      this.$api().post("/job?maxResults=" + this.countOfJobs, postBody).then(response => {
-        if (response.data != null && response.data.length > 0) {
-          response.data.forEach(element => {
-
-            this.jobsIds.push(element.id);
-          });
-          var postBodyJobsId = {
-            jobIds: this.jobsIds,
-            retries: 1
-          }
-          this.$api().post("/job/retries", postBodyJobsId).then(() => {
-
-            this.$notify({
-              group: "foo",
-              title: "Restarted " + this.jobsIds.length + " jobs",
-              type: "success"
+      this.$api()
+        .get("/incident?maxResults=" + this.countOfJobs)
+        .then((response) => {
+          if (response.data != null && response.data.length > 0) {
+            response.data.forEach((element) => {
+              if (
+                element.incidentType === "failedExternalTask" &&
+                element.configuration != null
+              ) {
+                this.jobsIds.push(element.configuration);
+              }
             });
-            this.getAllIncidents();
-          }).catch(error => {
-            this.$notify({
-              group: "foo",
-              title: "Fail",
-              text: error,
-              type: "error"
-            })
-          })
-        }
-      })
+            var postBodyJobsId = {
+              externalTaskIds: this.jobsIds,
+              retries: 1,
+            };
+            this.$api()
+              .put("external-task/retries", postBodyJobsId)
+              .then(() => {
+                this.$notify({
+                  group: "foo",
+                  title: "Restarted " + this.jobsIds.length + " jobs",
+                  type: "success",
+                });
+                this.getAllIncidents();
+              })
+              .catch((error) => {
+                this.$notify({
+                  group: "foo",
+                  title: "Fail",
+                  text: error,
+                  type: "error",
+                });
+              });
+          }
+        });
     },
     countErrorJobs: function (selectedItem) {
-      this.$api().get("job/count?activityId=" + selectedItem).then(response => {
-        this.countOfJobs = response.data.count;
-      })
-    }
+      this.$api()
+        .get("job/count?activityId=" + selectedItem)
+        .then((response) => {
+          this.countOfJobs = response.data.count;
+        });
+    },
   },
 
   mounted: function () {
     this.getAllIncidents();
   },
   computed: {
-    filterFailedActivity() {
-      var array = [];
-      this.incidents.forEach(element => {
-        array.push(element.activityId);
-      })
-
-      var uniqueArray = array.filter(function (item, pos) {
-        return array.indexOf(item) == pos;
-      })
-
-      return uniqueArray
+    filteredIncidents() {
+      return this.incidents.filter((item) => {
+        if (this.hideParentIncidents) {
+          return item.globalRoot === true;
+        } else {
+          return true; // Show all incidents
+        }
+      });
     },
-  }
+    totalIncidentCount() {
+      return this.filteredIncidents.length;
+    },
+  },
 };
 </script>
 
